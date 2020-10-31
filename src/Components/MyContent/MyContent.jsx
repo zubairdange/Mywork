fbe4ae8a1f6272318736007018b17671da7f1e18
zubairdange <71386093+zubairdange@users.noreply.github.com>
2020-10-31 15:48:44 +0530

    update validated form in tab 5

--- src/Components/MyContent/MyContent.jsx
+++ src/Components/MyContent/MyContent.jsx
@@ -4,6 +4,7 @@ import { Tab1 } from "./Tab1";
 import { Tab2 } from "./Tab2";
 import { Tab3 } from "./Tab3";
 import { Tab4 } from "./Tab4";
+import { Tab5 } from "./Tab5";
 export const MyContent = () => {
   return (
     <Tabs defaultActiveKey="tab1" id="uncontrolled-tab-example">
@@ -18,6 +19,9 @@ export const MyContent = () => {
       </Tab>
       <Tab eventKey="tab4" title="Tab 4">
         <Tab4 />
+      </Tab>
+      <Tab eventKey="tab5" title="Tab 5">
+        <Tab5 />
       </Tab>
     </Tabs>
   );
---
+++ src/Components/MyContent/Tab5.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ValidatedForm } from "../MyForm/ValidatedForm";
+export const Tab5 = () => {
+  return (
+    <div>
+      <p>Validated Form</p>;
+      <ValidatedForm />
+    </div>
+  );
+};
