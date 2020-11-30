06bf9bc737db27d70ded7a78936e82a2b49e69df
zubairdange <71386093+zubairdange@users.noreply.github.com>
2020-11-30 18:41:33 +0530

    update

--- src/Components/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-export const store = createStore(counter);
-return(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById(‘root’)
-   );
