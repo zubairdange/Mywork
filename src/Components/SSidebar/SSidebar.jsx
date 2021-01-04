d6970f250bd3592a381903d6880de572bbb9f2d8
zubairdange <71386093+zubairdange@users.noreply.github.com>
2021-01-04 21:17:17 +0530

    no change

--- src/Components/SSidebar/SSidebar.jsx
+++ src/Components/SSidebar/SSidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 export const SSidebar = () => {
@@ -7,9 +8,28 @@ export const SSidebar = () => {
       <Menu>
         <SubMenu title="UI Technologies">
           <MenuItem>ReactJS</MenuItem>
-          <MenuItem>JavaScript</MenuItem>
+          <MenuItem>
+            JavaScript
+            <Menu>
+              <SubMenu title="backend Technologies">
+                <MenuItem>php</MenuItem>
+                <MenuItem>node js</MenuItem>
+                <MenuItem>sql</MenuItem>
+                <MenuItem>oracle</MenuItem>
+              </SubMenu>
+            </Menu>
+          </MenuItem>
           <MenuItem>CSS</MenuItem>
           <MenuItem>HTML</MenuItem>
+        </SubMenu>
+      </Menu>
+
+      <Menu align-items=" center">
+        <SubMenu title="backend Technologies">
+          <MenuItem>php</MenuItem>
+          <MenuItem>node js</MenuItem>
+          <MenuItem>sql</MenuItem>
+          <MenuItem>oracle</MenuItem>
         </SubMenu>
       </Menu>
     </ProSidebar>
